b6169af877ea2b5e45a1637e684903cc6cafcd1c
liheng <1030330691@qq.com>
2022-11-04 11:23:40 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+# cp CNAME docs/.vuepress/dist/
+cd docs/.vuepress/dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:lihengma/cdp-open-platform.git master:gh-pages
+
+
+cd -
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:lihengma/cdp-open-platform.git master:master
